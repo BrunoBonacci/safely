ef577722a2be55353c956c0b74b99694fd2ee06c
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-09-19 16:21:30 +0100

    added randomized exponential backoff retry

--- src/safely/core.clj
+++ src/safely/core.clj
@@ -11,6 +11,8 @@
 ;; * handlers
 ;;
 
+(def ^:dynamic *no-sleep* false)
+
 
 (defun random
   ([:min min :max max]  (+ min (rand-int (- max min))))
@@ -18,20 +20,55 @@
                           (rand-between (- base variance) (+ base variance)))))
 
 
+(defn- exponential-seq
+  ([base max-value]
+   (map #(min % max-value) (exponential-seq base)))
+  ([base]
+   (let [base   (Math/abs base)
+         log10  (fn [n] (/ (Math/log n) (Math/log 10)))
+         pow    (fn [b p] (apply *' (repeat p b)))
+         size   (int (log10 base))
+         factor (pow 10 size)]
+     (map :value
+          (iterate (fn [{:keys [base size factor step] :as d}]
+                     (let [value (quot (*' factor (pow (/ base factor) (inc step))) 1)]
+                       (-> (assoc d :value value)
+                           (update :step inc))))
+                   {:size size :factor factor :base base :step 1 :value base})))))
+
+
 (defun sleep
-  ([n]              (try
-                      (Thread/sleep n)
-                      (catch Exception x#)))
+  ([n]              (when-not *no-sleep*
+                      (try
+                        (Thread/sleep n)
+                        (catch Exception x#))))
   ([:min a :max b]  (sleep (random :min a :max b)))
   ([b :+/- v]       (sleep (random b :+/- v))))
 
+
+(defun sleeper
+  ([:fix n]                      (fn [] (sleep n)))
+  ([:random b :+/- v]            (fn [] (sleep b :+/- v)))
+  ([:random-range :min l :max h] (fn [] (sleep :min l :max h)))
+
+  ([:random-exp-backoff :base b :+/- v]
+   (let [sleep-times (atom (cons 0 (exponential-seq b)))]
+     (fn []
+       (let [[t] (swap! sleep-times rest)]
+         (sleep t :+/- v)))))
+
+  ([:random-exp-backoff :base b :+/- v :max m]
+   (let [sleep-times (atom (cons 0 (exponential-seq b m)))]
+     (fn []
+       (let [[t] (swap! sleep-times rest)]
+         (sleep t :+/- v))))))
 
 
 (defn- make-attempt [f]
   (try
     [(f)]
     (catch Throwable x
-      (println x)
+      (println "ERROR::" (.getMessage x))
       [::got-error x])))
 
 
@@ -60,8 +97,5 @@
 
 (comment
   (safely-fn (fn [] (/ 1 0) (println "done")) :max-retry 3)
-
-  (def defaults {:max-retry 0 :attempt 0 :retry-delay 1000})
-  (merge defaults {:max-retry 3})
 
   )
