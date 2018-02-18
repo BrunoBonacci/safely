5300625e2cd61205fbc6a4565cadf0539939d4ce
Bruno Bonacci <bruno.bonacci@gmail.com>
2018-02-18 16:45:00 +0000

    release: 0.5.0-alpha4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.brunobonacci/safely "0.5.0-alpha3"
+(defproject com.brunobonacci/safely "0.5.0-alpha4"
   :description "safely it's a Clojure library for exception handling
   and offers a elegant declarative approach to the error management."
   :url "https://github.com/BrunoBonacci/safely"
