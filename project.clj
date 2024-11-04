2024bf248470e47b59f2de3fac7383c43b0d9992
Bruno Bonacci <bruno.bonacci@gmail.com>
2024-11-04 13:29:49 +0000

    updated dependencies and add clojure 1.12

--- project.clj
+++ project.clj
@@ -7,11 +7,11 @@
 
   :scm {:name "git" :url "https://github.com/BrunoBonacci/safely"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.match "1.0.0"]
-                 [defun "0.3.1"]
-                 [org.clojure/tools.logging "1.1.0"]
-                 [com.brunobonacci/mulog "0.4.0"]
+  :dependencies [[org.clojure/clojure "1.12.0" :scope "provided"]
+                 [org.clojure/core.match "1.1.0"]
+                 [defun "0.4.0"]
+                 [org.clojure/tools.logging "1.3.0"]
+                 [com.brunobonacci/mulog "0.9.0"]
                  [amalloy/ring-buffer "1.3.1"]]
 
   :global-vars {*warn-on-reflection* true}
@@ -19,19 +19,20 @@
   :jvm-opts ["-server"]
 
   :profiles {:dev {:resource-paths ["dev-resources"]
-                   :dependencies [[org.clojure/test.check "1.1.0"]
-                                  [midje "1.9.9"]
+                   :dependencies [[org.clojure/test.check "1.1.1"]
+                                  [midje "1.10.10"]
                                   [org.slf4j/slf4j-log4j12 "1.7.30"]
-                                  [com.brunobonacci/mulog-zipkin "0.4.0"]]
+                                  [com.brunobonacci/mulog-zipkin "0.9.0"]]
                    :plugins [[lein-midje "3.2.2"]]}
 
              :clj18  {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :clj19  {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :clj110 {:dependencies [[org.clojure/clojure "1.10.1"]]}
              :clj111 {:dependencies [[org.clojure/clojure "1.11.1"]]}
+             :clj112 {:dependencies [[org.clojure/clojure "1.12.0"]]}
              }
 
   :aliases {"test" ["do" "clean," "midje"]
-            "build-all"  ["with-profile" "+clj18:+clj19:+clj110:+clj111"
+            "build-all"  ["with-profile" "+clj18:+clj19:+clj110:+clj111:+clj112"
                           "do" "clean," "check," "midje," "jar"]}
   )
